b5d063d26e2d5ca60ebc5e33713a9ab48d7b05a6
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2022-04-21 15:47:33 +0300

    m13e1: handle shapes drawing

--- maman13/src/main/java/exercise1/Controller.java
+++ maman13/src/main/java/exercise1/Controller.java
@@ -2,40 +2,88 @@ package exercise1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Shape;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 
 public class Controller {
 
-    private Shape shape;
+    private Line line;
+    private Circle circle;
+    private Rectangle rectangle;
+    private String type = "line";
+    Group group = new Group();
 
     @FXML
     private Pane drawpane;
 
     @FXML
-    void onCircleClick(ActionEvent event) {
-
+    public void initialize() {
+        drawpane.getChildren().add(group);
     }
 
     @FXML
     void onLineClick(ActionEvent event) {
+        type = "line";
+    }
 
+    @FXML
+    void onCircleClick(ActionEvent event) {
+        type = "circle";
     }
 
     @FXML
     void onRectangleClick(ActionEvent event) {
-
+        type = "rectangle";
     }
 
     @FXML
-    void onDragDetected(MouseEvent event) {
-
+    void onMousePressed(MouseEvent event) {
+        switch (type) {
+            case "line":
+                line = new Line();
+                line.setStrokeWidth(2);
+                line.setFill(Color.BLACK);
+                line.setStartX(event.getX());
+                line.setStartY(event.getY());
+                break;
+            case "circle":
+                circle = new Circle();
+                circle.setStrokeWidth(2);
+                circle.setCenterX(event.getX());
+                circle.setCenterY(event.getY());
+                break;
+            case "rectangle":
+                rectangle = new Rectangle();
+                rectangle.setStrokeWidth(2);
+                rectangle.setX(event.getX());
+                rectangle.setY(event.getY());
+                break;
+        }
     }
 
     @FXML
-    void onDragDone(DragEvent event) {
-
+    void onMouseReleased(MouseEvent event) {
+        switch (type) {
+            case "line":
+                line.setEndX(event.getX());
+                line.setEndY(event.getY());
+                group.getChildren().add(line);
+                break;
+            case "circle":
+                circle.setRadius(event.getX() - circle.getCenterX());
+                group.getChildren().add(circle);
+                break;
+            case "rectangle":
+                rectangle.setWidth(event.getX() - rectangle.getX());
+                rectangle.setHeight(event.getY() - rectangle.getY());
+                group.getChildren().add(rectangle);
+                break;
+        }
     }
 }
