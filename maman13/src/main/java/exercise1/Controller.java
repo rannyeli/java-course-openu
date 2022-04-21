7768da324d5f36aa11a391bb550f0ffe147bbdc1
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2022-04-22 00:43:30 +0300

    m13e1:  improve ui and add more controls

--- maman13/src/main/java/exercise1/Controller.java
+++ maman13/src/main/java/exercise1/Controller.java
@@ -1,65 +1,91 @@
 package exercise1;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.Group;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 public class Controller {
 
     private Line line;
     private Circle circle;
     private Rectangle rectangle;
-    private String type = "line";
-    Group group = new Group();
+    private String type;
+    private Color color;
+    private final Group group = new Group();
+    private final ToggleGroup tgroup = new ToggleGroup();
 
     @FXML
     private Pane drawpane;
+    @FXML
+    private ComboBox<String> comboBox;
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private RadioButton fillRadioBtn;
+    @FXML
+    private RadioButton emptyRadioBtn;
 
     @FXML
     public void initialize() {
         drawpane.getChildren().add(group);
+        comboBox.setItems(FXCollections.observableArrayList("Line", "Circle", "Rectangle"));
+        comboBox.getSelectionModel().selectFirst();
+        fillRadioBtn.setToggleGroup(tgroup);
+        emptyRadioBtn.setToggleGroup(tgroup);
+        fillRadioBtn.setSelected(true);
+        type = comboBox.getValue();
+        color = colorPicker.getValue();
     }
 
     @FXML
-    void onLineClick(ActionEvent event) {
-        type = "line";
+    void handleShapeChange(ActionEvent event) {
+        type = comboBox.getValue();
     }
 
     @FXML
-    void onCircleClick(ActionEvent event) {
-        type = "circle";
+    void handleColorChange(ActionEvent event) {
+        color = colorPicker.getValue();
+    }
+
+    private void initShape(Shape shape) {
+        shape.setStrokeWidth(2);
+        shape.setStroke(color);
+        if (fillRadioBtn.isSelected())
+            shape.setFill(color);
+        else
+            shape.setFill(Color.TRANSPARENT);
     }
 
     @FXML
-    void onRectangleClick(ActionEvent event) {
-        type = "rectangle";
-    }
-
-    @FXML
-    void onMousePressed(MouseEvent event) {
+    void handleMousePressed(MouseEvent event) {
         switch (type) {
-            case "line":
+            case "Line":
                 line = new Line();
-                line.setStrokeWidth(2);
-                line.setFill(Color.BLACK);
+                initShape(line);
                 line.setStartX(event.getX());
                 line.setStartY(event.getY());
                 break;
-            case "circle":
+            case "Circle":
                 circle = new Circle();
-                circle.setStrokeWidth(2);
+                initShape(circle);
                 circle.setCenterX(event.getX());
                 circle.setCenterY(event.getY());
                 break;
-            case "rectangle":
+            case "Rectangle":
                 rectangle = new Rectangle();
-                rectangle.setStrokeWidth(2);
+                initShape(rectangle);
                 rectangle.setX(event.getX());
                 rectangle.setY(event.getY());
                 break;
@@ -67,22 +93,35 @@ public class Controller {
     }
 
     @FXML
-    void onMouseReleased(MouseEvent event) {
+    void handleMouseReleased(MouseEvent event) {
         switch (type) {
-            case "line":
+            case "Line":
                 line.setEndX(event.getX());
                 line.setEndY(event.getY());
                 group.getChildren().add(line);
                 break;
-            case "circle":
-                circle.setRadius(event.getX() - circle.getCenterX());
+            case "Circle":
+                circle.setRadius(Math.abs(event.getX() - circle.getCenterX()));
                 group.getChildren().add(circle);
                 break;
-            case "rectangle":
-                rectangle.setWidth(event.getX() - rectangle.getX());
-                rectangle.setHeight(event.getY() - rectangle.getY());
+            case "Rectangle":
+                rectangle.setWidth(Math.abs(event.getX() - rectangle.getX()));
+                rectangle.setHeight(Math.abs(event.getY() - rectangle.getY()));
                 group.getChildren().add(rectangle);
                 break;
         }
+    }
+
+    @FXML
+    void handleUndoClick(ActionEvent event) {
+        int size = group.getChildren().size();
+        if (size > 0) {
+            group.getChildren().remove(size - 1);
+        }
+    }
+
+    @FXML
+    void handleClearClick(ActionEvent event) {
+        group.getChildren().clear();
     }
 }
