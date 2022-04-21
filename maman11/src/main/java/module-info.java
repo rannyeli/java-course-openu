7c9691f9f31a54ab53a2f8379510530a00480737
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2022-04-21 12:22:39 +0300

    fix lint

--- maman11/src/main/java/module-info.java
+++ maman11/src/main/java/module-info.java
@@ -1,17 +1,18 @@
-// module exercise1 {
-// requires javafx.controls;
-// requires javafx.fxml;
-
-// opens exercise1 to javafx.fxml;
-
-// exports exercise1;
-// }
-
-module exercise2 {
+module exercise1 {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens exercise2 to javafx.fxml;
+    opens exercise1 to javafx.fxml;
 
-    exports exercise2;
+    exports exercise1;
 }
+// module exercise2 {
+// requires transitive javafx.graphics;
+// requires javafx.controls;
+// requires javafx.fxml;
+
+// opens exercise2 to javafx.fxml;
+
+// exports exercise2;
+// }
--- maman13/src/main/java/module-info.java
+++ maman13/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module exercise1 {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
     opens exercise1 to javafx.fxml;
+
     exports exercise1;
 }
