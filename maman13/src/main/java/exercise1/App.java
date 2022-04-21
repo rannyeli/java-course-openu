b977726914fa089e9c1a72bb77e50dbf0735ed43
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2022-04-21 14:28:04 +0300

    m13e1: add scene and controller

--- maman13/src/main/java/exercise1/App.java
+++ maman13/src/main/java/exercise1/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("ex1-scene"));
         stage.setScene(scene);
         stage.show();
     }
---
+++ maman13/src/main/java/exercise1/Controller.java
@@ -0,0 +1,41 @@
+package exercise1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Shape;
+
+public class Controller {
+
+    private Shape shape;
+
+    @FXML
+    private Pane drawpane;
+
+    @FXML
+    void onCircleClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLineClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRectangleClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDragDetected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onDragDone(DragEvent event) {
+
+    }
+}
