fa0dbf3a3a91bb9c5d09bde09506b647bb88d14f
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2022-04-23 22:05:30 +0300

    m13e2: create scene and controller

---
+++ maman13/src/main/java/exercise2/Controller.java
@@ -0,0 +1,59 @@
+package exercise2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+
+    @FXML
+    private GridPane gameBoard;
+
+    @FXML
+    private Label blueLabel;
+
+    @FXML
+    private Label redLabel;
+
+    @FXML
+    void handleColOne(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleColTwo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleColThree(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleColFour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleColFive(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleColSix(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleColSeven(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleClear(ActionEvent event) {
+
+    }
+
+}
--- maman13/src/main/java/module-info.java
+++ maman13/src/main/java/module-info.java
@@ -1,9 +1,18 @@
-module exercise1 {
+// module exercise1 {
+// requires transitive javafx.graphics;
+// requires javafx.controls;
+// requires javafx.fxml;
+
+// opens exercise1 to javafx.fxml;
+
+// exports exercise1;
+// }
+module exercise2 {
     requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens exercise1 to javafx.fxml;
+    opens exercise2 to javafx.fxml;
 
-    exports exercise1;
+    exports exercise2;
 }
