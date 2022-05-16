ba1867d220346701b65374c82b65b67266383b6a
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2022-05-16 21:34:00 +0300

    m14e2: init files

--- maman14/src/main/java/module-info.java
+++ maman14/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module exercise2 {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
     opens exercise2 to javafx.fxml;
+
     exports exercise2;
 }
