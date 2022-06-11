1cc36d3e71643e24c2a0fd9a2f83191c544f85db
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2022-06-11 21:02:33 +0300

    m15e2: create thread and main

--- maman15/src/exercise2/App.java
+++ maman15/src/exercise2/App.java
@@ -1,6 +1,6 @@
-package exercise1;
+package exercise2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,27 +11,23 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         final int MAX_VALUE = 100;
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter number of workers: ");
-        int m = sc.nextInt();
-        System.out.print("Please enter number of rounds: ");
         int n = sc.nextInt();
+        System.out.print("Please enter number of rounds: ");
+        int m = sc.nextInt();
         sc.close();
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
         Random rand = new Random();
-        for (int i = 0; i < n; i++) { // generate values for list
-            list.add(i, rand.nextInt(MAX_VALUE));
-        }
-        System.out.println("The list: " + list);
-        Monitor mon = new Monitor(list);
-        Worker[] workers = new Worker[m];
+        Worker[] workers = new Worker[n];
         for (int i = 0; i < workers.length; i++) { // init workers
-            workers[i] = new Worker(mon);
+            workers[i] = new Worker(rand.nextInt(MAX_VALUE), i, workers, m);
         }
+        System.out.println(Arrays.toString(workers));
         for (int i = workers.length - 1; i >= 0; i--) { // start workers
             workers[i].start();
         }
-        System.out.println("Total sum: " + mon.getResult());
+
     }
 }
---
+++ maman15/src/exercise2/Worker.java
@@ -0,0 +1,98 @@
+package exercise2;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Worker extends Thread {
+    private Integer _id;
+    private int _i;
+    private int _leftI;
+    private int _rightI;
+    private Worker[] _workers;
+    private int _m;
+    private int _checked = 0;
+    private int _round = 1;
+    private static int _done = 0;
+    private static Lock lock = new ReentrantLock();
+    private static Condition cond = lock.newCondition();
+
+    public Worker(int id, int i, Worker[] workers, int m) {
+        this._id = id;
+        this._i = i;
+        this._workers = workers;
+        this._m = m;
+
+        if (_i == 0) {
+            _leftI = _workers.length - 1;
+            _rightI = _i + 1;
+        } else if (_i == _workers.length - 1) {
+            _leftI = _i - 1;
+            _rightI = 0;
+        } else {
+            _leftI = _i - 1;
+            _rightI = _i + 1;
+        }
+    }
+
+    public synchronized int getWorkerId() {
+        _checked++;
+        notifyAll();
+        return _id;
+    }
+
+    public void process() {
+        int leftId = _workers[_leftI].getWorkerId();
+        int rightId = _workers[_rightI].getWorkerId();
+        synchronized (this) {
+            while (_checked == 0 || _checked % 2 != 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (_id < leftId && _id < rightId) {
+                _id++;
+            } else if (_id > leftId && _id > rightId) {
+                _id--;
+            }
+            _done++;
+        }
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (_round <= _m) {
+            process();
+
+            lock.lock();
+            try {
+                while (_done % _m != 0) {
+                    cond.await();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+
+            lock.lock();
+            try {
+                if (_done % _m == 0) {
+                    System.out.println(Arrays.toString(_workers));
+                    _round++;
+                    cond.signalAll();
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public String toString() {
+        return _id.toString();
+    }
+}
